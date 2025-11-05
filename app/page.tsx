87cda9d049579b7f253fa4082b387f927613b392
dulshanchamika <dulshanchamika2@gmail.com>
2025-11-06 00:22:18 +0530

    created home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import events from "@/lib/constants";
+
 const Page = () => {
   return (
     <section>
       <h1 className="text-center">The HUB for Every Dev <br /> Event you can't Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in one Place</p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
