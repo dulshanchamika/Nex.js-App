3d662a759313f72a218b2ca7bc0dd71a146bd432
dulshanchamika <dulshanchamika2@gmail.com>
2025-11-06 16:01:27 +0530

    created api routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import events from "@/lib/constants";
+import { IEvent } from "@/database/event.model";
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`, {
+    next: { revalidate: 60 },
+  });
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">The HUB for Every Dev <br /> Event you can't Miss</h1>
@@ -13,7 +20,7 @@ const Page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events list-none">
-          {events.map((event) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title}>
               <EventCard {...event} />
             </li>
